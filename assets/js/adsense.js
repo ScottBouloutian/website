2cae08a07316e2d6129287dfaa80b7629e139dc4
Scott Bouloutian <ScottBouloutian@gmail.com>
2022-01-15 17:05:56 -0500

    initialize fresh jekyll project

--- assets/js/adsense.js
+++
@@ -1,6 +0,0 @@
-const [head] = document.getElementsByTagName('head');
-const script = document.createElement('script');
-script.async = true;
-script.setAttribute('data-ad-client', 'ca-pub-4400088783500800');
-script.setAttribute('src', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
-head.appendChild(script);
